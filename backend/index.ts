6836354451f3ef2f7594793e30bc09c0eb854833
Dooor <dooor1109@gmail.com>
2019-02-23 12:11:53 +0900

    backend/index.ts修正

--- backend/index.ts
+++ backend/index.ts
@@ -16,12 +16,16 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const PORT = normalizePort(process.env.PORT || 3000);
 
+const onPrepare = async () => {
+  server.use('/', router(app));
+  server.listen(PORT);
+};
+
+const onError = (ex) => {
+  Logger.error(ex.stack);
+  process.exit(1);
+};
+
 app.prepare()
-  .then(async () => {
-    server.use('/', router(app));
-    server.listen(PORT);
-  })
-  .catch((ex) => {
-    Logger.error(ex.stack);
-    process.exit(1);
-  });
+  .then(onPrepare)
+  .catch(onError);
